1b07efe7b98e25aedab88a559bcda6c30f4a4b97
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-10-19 16:21:58 +0300

    fix: likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -126,6 +126,11 @@ function App() {
     }
   }, [loggedIn]);
 
+
+  // useEffect(() => {
+  //   checkToken();
+  // },[]);
+
   // useEffect(() => {
   //   if (loggedIn) {
   //     Promise.all([api.getProfile(), api.getInitialCards()])
@@ -208,7 +213,7 @@ function App() {
   };
 
   const handleCardLike = (card) => {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     api
       .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
