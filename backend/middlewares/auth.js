1edc94409f0de3d0447572a29eac1fad899915c1
Margarita Shabanova <marg.alexandrova2018@yandex.ru>
2022-10-20 15:24:05 +0300

    fix: edit auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
-const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzRmZThkNzkzNzkwOGE1ZTRhMDE4OWIiLCJpYXQiOjE2NjYyNjgyMDQsImV4cCI6MTY2Njg3MzAwN'; // вставьте сюда JWT, который вернул публичный сервер
-const SECRET_KEY_DEV = '22a981272b0aa91febfb9af970be58e4936c3fbac51b5e46c775edb157d1289b';
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
@@ -13,32 +11,12 @@ module.exports = (req, res, next) => {
   }
 
   let payload;
-  try {
-    payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-    console.log('\x1b[31m%s\x1b[0m', `
-    Надо исправить. В продакшне используется тот же
-    секретный ключ, что и в режиме разработки.
-    `);
-  } catch (err) {
-    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-      console.log(
-        '\x1b[32m%s\x1b[0m',
-        'Всё в порядке. Секретные ключи отличаются',
-      );
-    } else {
-      console.log(
-        '\x1b[33m%s\x1b[0m',
-        'Что-то не так',
-        err,
-      );
-    }
-  }
 
-  // try {
-  //   payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
-  // } catch (err) {
-  //   throw new UnauthorizedError();
-  // }
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+  } catch (err) {
+    throw new UnauthorizedError();
+  }
 
   req.user = payload;
   next();
